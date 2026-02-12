caebe774158b76f8e007b9393d5b95f8cd486789
Shiro <66051179@kmitl.ac.th>
2026-02-12 21:57:54 +0700

    REAL FINAL./main.py | cat -e!

--- rush/ex00/checkmate.py
+++ rush/ex00/checkmate.py
@@ -1,53 +1,53 @@
-#!/usr/bin/env python3
+# board structure example 4x4 :
+# y\x ---------------------> +
+#  | [0,0] [0,1] [0,2] [0,3]
+#  | [1,0] [1,1] [1,2] [1,3]
+#  V [2,0] [2,1] [2,2] [2,3]
+#  + [3,0] [3,1] [3,2] [3,3]
 
+# is_safe function used to check if the position is inside board.
 def is_safe(pos_x, pos_y, max_x, max_y):
-    return 0 <= pos_x < max_x and 0 <= pos_y < max_y  #FIXED
+    return 0 <= pos_x < max_x and 0 <= pos_y < max_y
 
+# get_moves function used to get all possible move for pieces.
 def get_moves(piece, pos_x, pos_y, board, max_x, max_y):
-
     moves = []
     directions = []
-
     
     if piece == 'P':
         directions = [(-1, -1), (-1, 1)] 
-
-        for dy, dx in directions:
-            new_y, new_x = pos_y + dy, pos_x + dx
+        for dir_y, dir_x in directions:
+            new_y, new_x = pos_y + dir_y, pos_x + dir_x
             if is_safe(new_x, new_y, max_x, max_y):
                 moves.append((new_y, new_x))
         return moves
-
-    elif piece == 'B': 
-        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)] 
-    elif piece == 'R': 
+    elif piece == 'B':
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    elif piece == 'R':
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    elif piece == 'Q': 
+    elif piece == 'Q':
         directions = [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (1, 0), (0, -1), (0, 1)]
 
-
-    for dy, dx in directions:
-        new_y, new_x = pos_y + dy, pos_x + dx
+    for dir_y, dir_x in directions:
+        new_y, new_x = pos_y + dir_y, pos_x + dir_x
         while is_safe(new_x, new_y, max_x, max_y):
             moves.append((new_y, new_x))
-            
             if board[new_y][new_x] != '.':
                 break
-            
-            new_y += dy
-            new_x += dx           
+            new_y += dir_y
+            new_x += dir_x
     return moves
 
-
+# checkmate function used to check if King can be captured by another pieces.
 def checkmate(board_str):
     try:
         clean_board_str = board_str.strip()
         if not clean_board_str:
-            print("Empty Board")  #FIXED
+            print("Empty Board")
             return
 
+        #Extract board_str into list.
         raw_lines = clean_board_str.splitlines()
-
         rows = []
         for line in raw_lines:
             clean_line = line.strip()
@@ -56,55 +56,45 @@ def checkmate(board_str):
         height = len(rows)
         width = len(rows[0])
         
-        # 1. เช็คว่าทุกแถวยาวเท่ากันไหม
+        # Check if board is valid
         for row in rows:
-            if len(row) != width:
-                print("Square Not Complete") #FIXED
+            if len(row) != width or height != width:
+                print("Table is not square") #FIXED
                 return
 
-        # 2. เช็คว่าเป็นจัตุรัสไหม (กว้าง == สูง)
-        if height != width:
-            print("TABLE IS NOT SQUARE!!") #FIXED
-            return
+        kings_found = []
+        enemies = []
 
-        # เก็บ List ของ King 
-        kings_found = [] 
-        enemies = [] 
-
+        # Pieces Validation
         for r in range(height):
             for c in range(width):
                 piece = rows[r][c]
                 if piece == 'K':
-                    kings_found.append((r, c)) # เจอ K ให้เก็บพิกัดไว้
+                    kings_found.append((r, c))
                 elif piece in ['P', 'B', 'R', 'Q']:
                     enemies.append((piece, r, c))
                 elif piece != '.':
                     print("Error, Found illegal piece") 
                     return
 
-        # FIND King 
+        # Validate King Amount
         king_count = len(kings_found)
-        
         if king_count == 0:
-            print("Don't have KING on chess board")
+            print("Don't have any King on chess board")
             return
-            
         if king_count > 1:   
             excess = king_count - 1
             print(f"Error: Multiple Kings found. Excess: {excess}")
             return
 
-        # ดึงตำแหน่ง King ตัวจริงออกมา 
-        king_pos = kings_found[0] #0 is pos of Real KING
-
-        # --- คำนวณการรุกฆาต ---
+        # Check if King is in check
+        king_pos = kings_found[0]
         for piece, r, c in enemies:
             possible_moves = get_moves(piece, c, r, rows, width, height)
             if king_pos in possible_moves:
                 print("Success")
                 return
-
         print("Fail")
 
     except Exception:
-        print("Fail")
+        print("Error Exception")
--- rush/ex00/main.py
+++ rush/ex00/main.py
@@ -2,35 +2,70 @@
 from checkmate import checkmate
 
 def main():
-    # Test 1: มี King 2 ตัว (เกินมา 1)
-    print("--- Test 1: 2 Kings (Excess 1) ---")
-    board_2k = """\
-K...
-.K..
-...
+    board = """\
+R... 
+.K.. 
+..P. 
 ....\
 """
-    checkmate(board_2k)
+#     board_empty =""
+#     board_not_sq="""\
+# K...
+# .K..
+# ...\
+# """
+#     board_2k = """\
+# K...
+# .K..
+# ....
+# ....\
+# """
+#     board_3k = """\
+# K...
+# .K..
+# ..K.
+# ....\
+# """
+#     board_normal_2x2 = """\
+# KQ
+# BB\
+# """
+#     board_normal_3x3 = """\
+# PPQ
+# RKB
+# RPB\
+# """
+#     board_normal_4x4 = """\
+# .P.R
+# BPPP
+# PRQK
+# BPPP\
+# """
+#     board_normal_5x5 = """\
+# ..PP.
+# ...RP
+# .B.P.
+# KRB.P
+# .QPPP
+# """
+#     print("--- Test 1: Empty ---")
+#     checkmate(board_empty)
+#     print("\n--- Test 2: Not Square ---")
+#     checkmate(board_not_sq)
+#     print("\n--- Test 3: 2 King ---")
+#     checkmate(board_2k)
+#     print("\n--- Test 4: 3 King ---")
+#     checkmate(board_3k)
+#     print("\n--- Test 5: Normal 2x2 ---")
+#     checkmate(board_normal_2x2)
+#     print("\n--- Test 6: Normal 3x3 ---")
+#     checkmate(board_normal_3x3)
+#     print("\n--- Test 7: Normal 4x4 ---")
+#     checkmate(board_normal_4x4)
+#     print("\n--- Test 8: Normal 5x5 ---")
+#     checkmate(board_normal_5x5)
 
-    # Test 2: มี King 3 ตัว (เกินมา 2)
-    print("\n--- Test 2: 3 Kings (Excess 2) ---")
-    board_3k = """\
-
-
-
-
-"""
-    checkmate(board_3k)
-
-    # Test 3: ปกติ (King 1 ตัว)
-    print("\n--- Test 3: Normal (1 King) ---")
-    board_normal = """\
-
-.K..
-R...
-....\
-"""
-    checkmate(board_normal)
+    checkmate(board)
 
 if __name__ == "__main__":
     main()
